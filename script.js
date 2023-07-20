96032b82abd95249ac4845edf230a6982fba59a2
hanzadahannie <hanzadaakylbekovna@gmail.com>
2023-07-20 01:50:46 -0700

    hm w

---
+++ script.js
@@ -0,0 +1,16 @@
+const handleResponse = () => {
+    fetch('https://swapi.dev/api/people/1')
+        .then(response => response.json())
+        .then(data => {
+            fetch('https://swapi.dev/api/vehicles/14/')
+                .then(response => response.json())
+                .then(vehicles => {
+                    fetch("https://swapi.dev/api/starships/12/")
+                        .then(response => response.json())
+                        .then(starships => alert(`Hello ${data.name} ${data.birth_year} ${vehicles.name} ${starships.name}`))
+                })
+        })
+        .catch(error => console.log(error))
+}
+
+handleResponse()
